80cbb48e2a37ade2a98ed3b0f09c7742adea2871
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-03-05 15:34:07 +0000

    feat: Improved grid border

--- src/lib/PixelGrid.svelte
+++ src/lib/PixelGrid.svelte
@@ -38,8 +38,9 @@
 <div class="relative">
   <div
     class="grid select-none"
-    style="grid-template-columns: repeat({rows}, minmax(0, 1fr)); grid-template-rows: repeat({cols}, minmax(0, 1fr)); transform: translateZ(0);"
+    style="grid-template-columns: repeat({rows}, minmax(0, 1fr)); grid-template-rows: repeat({cols}, minmax(0, 1fr)); transform: translateZ(0); gap: 0;"
   >
+
     {#each layers[0].grid as row, i}
       {#each row as pixel, j}
         <SinglePixel
@@ -54,20 +55,18 @@
       {/each}
     {/each}
   </div>
+
   {#if borders}
-    <div class="absolute inset-0 overflow-hidden select-none pointer-events-none opacity-30">
-      <svg class="absolute inset-[-0.5px]" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+    <div class="absolute inset-0 overflow-hidden select-none pointer-events-none opacity-100 mix-blend-difference">
+      <svg class="absolute inset-[-1px]" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
         <defs>
           <pattern id="grid" width="{100 / rows}%" height="{100 / cols}%" patternUnits="userSpaceOnUse">
-            <path d="M 160 0 L 0 0 0 160" fill="none" stroke-dasharray="3,3" stroke="white" stroke-width="1"/>
-          </pattern>
-          <pattern id="grid2" width="{100 / rows}%" height="{100 / cols}%" patternUnits="userSpaceOnUse">
-            <path d="M 80 0 L 0 0 0 80" fill="none" stroke-dasharray="3,3" stroke="black" stroke-width="1"/>
+            <path d="M 1000 0 L 0 0 0 1000" fill="none" stroke-dasharray="3,3" stroke="white" stroke-width="1"/>
           </pattern>
         </defs>
         <rect width="100%" height="100%" fill="url(#grid)" class="" />
-        <rect width="100%" height="100%" fill="url(#grid2)" class="" />
       </svg>
     </div>
   {/if}
+
 </div>
